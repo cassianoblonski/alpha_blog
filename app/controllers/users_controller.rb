e7c0fc53ff81d4ed0246dd25e42993881f89a9bc
Cassiano Blonski Sampaio <cassianobsampaio@gmail.com>
2019-03-18 14:44:31 -0300

    Signup users from UI

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController <  ApplicationController
+  # before_action :set_user, only: [:edit, :update, :show, :destroy]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the alpha blog #{@user.username}"
+      # redirect_to user_path(@user)
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    # def set_user
+    #   @user = User.find(params[:id])
+    # end
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :articles
+  has_secure_password
   before_save { self.email = email.downcase }
   validates :username, presence: true,
             uniqueness: { case_sensitive: false },
@@ -9,5 +10,4 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 105 },
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
-  has_secure_password
 end
